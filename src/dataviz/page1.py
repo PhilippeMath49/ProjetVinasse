28c0425ec434399d8c0db87bac7305410f171190
NoaYnov <113116077+NoaYnov@users.noreply.github.com>
2025-02-20 15:17:52 +0100

    page

---
+++ src/dataviz/page1.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+import geopandas as gpd
+import os
+
+# Charger les données
+class Page1:
+    wine_prod_df = pd.read_csv("../data/wine-production/wine-production.csv")
+    reviews_df = pd.read_csv("../data/winemag.csv")
+
+    # Nettoyage et traitement
+    def clean_wine_data(df):
+        df.dropna(subset=["Entity", "Wine"], inplace=True)
+        df["Year"] = df["Year"].astype(int)
+        return df.groupby("Entity")["Wine"].median().reset_index()
+    
+    def general():
+
+        wine_prod_df = Page1.clean_wine_data(wine_prod_df)
+
+        # Charger les données géographiques
+        shapefile_path = "../map/ne_110m_admin_0_countries/ne_110m_admin_0_countries.shp"
+        world = gpd.read_file(shapefile_path)
+
+        # Fusion des données
+        world = world.merge(wine_prod_df, left_on="NAME", right_on="Entity", how="left")
+
+        # Création de la carte interactive
+        fig_map = px.choropleth(world, geojson=world.geometry, locations=world.index, color="Wine",
+                                hover_name="Entity", title="Production de Vin par Pays")
+        fig_map.update_geos(fitbounds="locations", visible=False)
+
+        # Création d'un histogramme des notes de vin
+        fig_hist = px.histogram(Page1.reviews_df, x="points", nbins=20, title="Distribution des Notes de Vin")
+
+        # Création de l'application Streamlit
+        st.title("Tableau de Bord sur le Vin")
+
+        st.subheader("Production de Vin par Pays")
+        st.plotly_chart(fig_map)
+
+        st.subheader("Distribution des Notes de Vin")
+        st.plotly_chart(fig_hist)
+        
+        
+        
+        
--- src/main.py
+++ src/main.py
@@ -3,16 +3,15 @@ import pandas as pd  # Pour les manipulations de données
 import numpy as np  # Pour les calculs numériques
 import matplotlib.pyplot as plt  # Pour les graphiques
 import seaborn as sns  # Pour les visualisations
-from lib.data import Data  
+import os 
 
 
-def Wine_Prod():
-    wineprod_df = Data.data_extraction("data/wine-production/wine-production.csv")
-    wineprod_treated_df = Data.wine_prod_treatment(wineprod_df)
-    Data.wine_prod_prompt(wineprod_treated_df)
+
 
 def main():
-    Wine_Prod()
+    os.system("python -m streamlit run dataviz/page1.py")
+    
+    
 
 
 
