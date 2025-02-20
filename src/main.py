cc2e123be365d3997e6578429fa1012632aae1f3
NoaYnov <113116077+NoaYnov@users.noreply.github.com>
2025-02-20 16:00:21 +0100

    👈(ﾟヮﾟ👈)

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ import dataviz.page1 as page1  # Pour les visualisations
 
 
 def main():
-    page1.Page1.general()
+    page1.Page1.general("./data/wine-production/wine-production.csv", "./data/winemag.csv")
     
     
     
