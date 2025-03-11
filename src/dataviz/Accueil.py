b27aeced4e8745c9bc9e934a3869d7fce44be79a
NoaYnov <113116077+NoaYnov@users.noreply.github.com>
2025-03-11 08:44:20 +0100

    Update Accueil.py

--- src/dataviz/Accueil.py
+++ src/dataviz/Accueil.py
@@ -31,11 +31,12 @@ def accueil():
     if st.button("Commencer l'Analyse"):
         # Cette fonction pourrait rediriger vers une autre page (exemple: page1)
         st.write("Introduction à l'Analyse des Données")
-        intro.main()
+        
+        page1.general()
     if st.button("INTRO"):
         # Cette fonction pourrait rediriger vers une autre page (exemple: page1)
         st.write("Vous êtes maintenant prêt à commencer l'analyse. Bonne exploration !")
-        page1.general()
+        intro.main()
 
     # Ajouter un peu de style personnalisé
     st.markdown("""
